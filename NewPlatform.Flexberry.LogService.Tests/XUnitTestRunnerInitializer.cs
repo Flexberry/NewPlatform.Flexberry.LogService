54102ba53eda174a209616adea6164c4e319fcb9
Tsar Nikolay <nsmirnov@ics.perm.ru>
2020-08-28 12:19:41 +0500

    netcoreapp3.1 test runner.

---
+++ NewPlatform.Flexberry.LogService.Tests/XUnitTestRunnerInitializer.cs
@@ -0,0 +1,32 @@
+﻿[assembly: Xunit.TestFramework("NewPlatform.Flexberry.LogService.Tests.XUnitTestRunnerInitializer", "NewPlatform.Flexberry.LogService.Tests")]
+
+namespace NewPlatform.Flexberry.LogService.Tests
+{
+#if NETCORE
+    using System.Configuration;
+    using System.IO;
+    using System.Reflection;
+#endif
+    using Xunit.Abstractions;
+    using Xunit.Sdk;
+
+    /// <summary>
+    /// Инициализация тестового запуска.
+    /// </summary>
+    public class XUnitTestRunnerInitializer : XunitTestFramework
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XUnitTestRunnerInitializer" /> class.
+        /// </summary>
+        /// <param name="messageSink">The message sink used to send diagnostic messages.</param>
+        public XUnitTestRunnerInitializer(IMessageSink messageSink)
+            : base(messageSink)
+        {
+#if NETCORE
+            string configFile = $"{Assembly.GetExecutingAssembly().Location}.config";
+            string outputConfigFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).FilePath;
+            File.Copy(configFile, outputConfigFile, true);
+#endif
+        }
+    }
+}
