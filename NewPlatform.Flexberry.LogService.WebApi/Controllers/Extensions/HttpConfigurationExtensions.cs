1b2bc66b099f8ecf5714e8cdd4f82ebbb77213a7
Churekov Valentin <VChurekov@ics.perm.ru>
2023-03-10 00:42:16 +0500

    Log netFramework.

---
+++ NewPlatform.Flexberry.LogService.WebApi/Controllers/Extensions/HttpConfigurationExtensions.cs
@@ -0,0 +1,40 @@
+﻿#if NETFRAMEWORK
+namespace ICSSoft.STORMNET.Controllers.Extensions
+{
+    using System;
+    using System.Web.Http;
+
+    /// <summary>
+    /// Класс, содержащий расширения для сервиса логирования.
+    /// </summary>
+    public static class HttpConfigurationExtensions
+    {
+        /// <summary>
+        /// Maps the specified Log Service route.
+        /// </summary>
+        /// <param name="config">The current HTTP configuration.</param>
+        /// <param name="routeName">The name of the route.</param>
+        public static void MapLogControllerRoute(
+            this HttpConfiguration config,
+            string routeName = "logs")
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config), "Contract assertion not met: config != null");
+            }
+
+            if (routeName == null)
+            {
+                throw new ArgumentNullException(nameof(routeName), "Contract assertion not met: routeName != null");
+            }
+
+            if (routeName == string.Empty)
+            {
+                throw new ArgumentException("Contract assertion not met: routeName != string.Empty", nameof(routeName));
+            }
+
+            config.Routes.MapHttpRoute(routeName, "api/logs", defaults: new { controller = "Logs", action = "PostLog" });
+        }
+    }
+}
+#endif
--- NewPlatform.Flexberry.LogService.WebApi/Controllers/LogsController.cs
+++ NewPlatform.Flexberry.LogService.WebApi/Controllers/LogsController.cs
@@ -5,6 +5,14 @@
 #if NETSTANDARD
     using Microsoft.AspNetCore.Mvc;
 #endif
+#if NETFRAMEWORK
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+#endif
 
     /// <summary>
     /// WebAPI controller for Flexberry Log Service.
@@ -32,6 +40,29 @@
             }
 
             return Ok();
+        }
+    }
+#endif
+#if NETFRAMEWORK
+    [RoutePrefix("api/logs")]
+    public class LogsController : ApiController
+    {
+        /// <summary>
+        /// Send log message for LogService.
+        /// </summary>
+        /// <param name="request">Log object.</param>
+        [HttpPost]
+        [ActionName("PostLog")]
+        public void PostLog([FromBody] LogRequest request)
+        {
+            try
+            {
+                LogService.LogDebug(request.LogMessage);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
         }
     }
 #endif
