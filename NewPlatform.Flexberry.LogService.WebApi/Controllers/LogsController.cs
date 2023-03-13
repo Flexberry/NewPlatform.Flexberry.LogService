2e15f54ee2ac20a5a4c266fa1adf1cc5cc304ef3
Churekov Valentin <VChurekov@ics.perm.ru>
2023-03-13 17:58:32 +0500

    LogRequest extended.

--- NewPlatform.Flexberry.LogService.WebApi/Controllers/LogsController.cs
+++ NewPlatform.Flexberry.LogService.WebApi/Controllers/LogsController.cs
@@ -6,11 +6,7 @@
     using Microsoft.AspNetCore.Mvc;
 #endif
 #if NETFRAMEWORK
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Net;
-    using System.Net.Http;
     using System.Web.Http;
 #endif
 
@@ -20,7 +16,13 @@
 #if NETSTANDARD
     [Produces("application/json")]
     public class LogsController : ControllerBase
+#endif
+#if NETFRAMEWORK
+    [RoutePrefix("api/logs")]
+    public class LogsController : ApiController
+#endif
     {
+#if NETSTANDARD
         /// <summary>
         /// Send log message for LogService.
         /// </summary>
@@ -29,24 +31,8 @@
         [HttpPost]
         [ActionName("PostLog")]
         public StatusCodeResult PostLog([FromBody] LogRequest request)
-        {
-            try
-            {
-                LogService.LogDebug(request.LogMessage);
-            }
-            catch
-            {
-                return BadRequest();
-            }
-
-            return Ok();
-        }
-    }
 #endif
 #if NETFRAMEWORK
-    [RoutePrefix("api/logs")]
-    public class LogsController : ApiController
-    {
         /// <summary>
         /// Send log message for LogService.
         /// </summary>
@@ -54,16 +40,43 @@
         [HttpPost]
         [ActionName("PostLog")]
         public void PostLog([FromBody] LogRequest request)
+#endif
         {
             try
             {
-                LogService.LogDebug(request.LogMessage);
+                switch (request.Category.ToLower())
+                {
+                    case "info":
+                        LogService.LogInfo(request.Message);
+                        break;
+                    case "debug":
+                        LogService.LogDebug(request.Message);
+                        break;
+                    case "warn":
+                    case "warning":
+                        LogService.LogWarn(request.Message);
+                        break;
+                    case "error":
+                        LogService.LogDebug(request.Message);
+                        break;
+                    default:
+                        LogService.LogInfo(request.Message);
+                        break;
+                }
             }
             catch
             {
+#if NETSTANDARD
+                return BadRequest();
+#endif
+#if NETFRAMEWORK
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
+#endif
             }
+
+#if NETSTANDARD
+            return Ok();
+#endif
         }
     }
-#endif
 }
--- NewPlatform.Flexberry.LogService.WebApi/Controllers/RequestsObjects/LogRequest.cs
+++ NewPlatform.Flexberry.LogService.WebApi/Controllers/RequestsObjects/LogRequest.cs
@@ -1,7 +1,35 @@
 ﻿namespace ICSSoft.STORMNET.RequestsObjects
 {
+    using System;
+
     public class LogRequest
     {
-        public string LogMessage { get; set; }
+        public string Category { get; set; }
+
+        public int? EventId { get; set; }
+
+        public int? Priority { get; set; }
+
+        public string Severity { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? TimeStamp { get; set; }
+
+        public string MachineName { get; set; }
+
+        public string AppDomainName { get; set; }
+
+        public string ProcessId { get; set; }
+
+        public string ProcessName { get; set; }
+
+        public string ThreadName { get; set; }
+
+        public string Win32ThreadId { get; set; }
+
+        public string Message { get; set; }
+
+        public string FormattedMessage { get; set; }
     }
 }
